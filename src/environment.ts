bd3e5716ca578e6405ff709109c64517bfa270e8
mrlldd <roflanjaraxxus@gmail.com>
2024-12-14 18:02:05 +0200

    try explicitly load env

--- src/environment.ts
+++ src/environment.ts
@@ -1,12 +1,16 @@
 import * as zod from 'zod';
+import { loadEnvConfig } from '@next/env';
+
+loadEnvConfig(process.cwd());
 
 const schema = zod.object({
 	environment: zod.enum(['local', 'production']),
 	version: zod.string().nonempty(),
 });
 
-console.log(process.env);
-
-const env = schema.parse(process.env);
+const env = schema.parse({
+	environment: process.env.QETRA_ENVIRONMENT,
+	version: process.env.QETRA_VERSION,
+});
 
 export default env;
