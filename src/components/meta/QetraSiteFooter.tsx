2c809858809a97238df2adffd973b722ba284a9d
mrlldd <roflanjaraxxus@gmail.com>
2024-12-16 16:17:09 +0200

    fix selects in footer

--- src/components/meta/QetraSiteFooter.tsx
+++ src/components/meta/QetraSiteFooter.tsx
@@ -3,7 +3,9 @@ import env from '@/environment';
 export default function QetraSiteFooter() {
 	return (
 		<footer className="fixed bottom-0">
-			<div className="invisible md:visible z-50 text-gray-700">App version: {env.version}</div>
+			<div className="invisible md:visible z-50 text-gray-700 select-none">
+				App version: <span className="select-text">{env.version}</span>
+			</div>
 		</footer>
 	);
 }
