c8219485565148c841e4d99a0c3eb2543c0a6e84
Yaroslav Tymchenko <ytimchenko57@gmail.com>
2025-05-28 18:09:34 +0300

    Typewriter component minor change

--- src/components/QetraTypewriter.tsx
+++ src/components/QetraTypewriter.tsx
@@ -4,11 +4,12 @@ import { Typewriter } from 'react-simple-typewriter';
 
 interface Props {
 	sentences: string[];
+	className?: string;
 }
 
-export default function QetraTypewriter({ sentences }: Readonly<Props>) {
+export default function QetraTypewriter({ sentences, className }: Readonly<Props>) {
 	return (
-		<span className={`text-base font-semibold text-purple-300 p-4`}>
+		<span className={`text-base font-semibold text-purple-300 p-4 ${className}`}>
 			<Typewriter words={sentences} cursorBlinking loop cursor typeSpeed={70} deleteSpeed={50} delaySpeed={1500} />
 		</span>
 	);
