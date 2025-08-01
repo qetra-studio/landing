20630b44eceb111a871532b41920bc9b48624ac9
Yaroslav Tymchenko <ytimchenko57@gmail.com>
2025-08-01 23:54:01 +0300

    improve arrow card in responsibilities section

--- src/app/(public)/components/responsibilities/info-section/index.tsx
+++ src/app/(public)/components/responsibilities/info-section/index.tsx
@@ -1,11 +1,20 @@
 import Grid, { GridItem } from '@/components/layout/grid';
 import composeClassName from '@/utils/compose-class-name';
-import { SquareArrow } from '@/components/ui/SquareArrow';
 import { ArrowUp } from 'lucide-react';
 
 const ResponsibilitiesItemCard = ({ title, className }: { title: string; className?: string }) => (
-	<GridItem className={composeClassName(`card card-border bg-card-dark m-4 grid-rows-2 rounded-4xl`, className)}>
-		<div className="card m-4 w-min bg-gradient-to-tr from-gray-700 to-gray-900 blur-[14.3]">
+	<GridItem
+		className={composeClassName(
+			`card card-border bg-card-dark m-4 grid-rows-2 rounded-4xl backdrop-blur-md`,
+			className
+		)}
+	>
+		<div
+			className="card m-4 w-min border-[0.5px] border-[#68686a]"
+			style={{
+				background: 'linear-gradient(135deg, #2c2d31 0%, #3CCF9E 50%, #2c2d31 100%)',
+			}}
+		>
 			<ArrowUp size={28} className="rotate-45" />
 		</div>
 		<span className="m-4 text-lg leading-[1.02] font-semibold text-white">{title}</span>
